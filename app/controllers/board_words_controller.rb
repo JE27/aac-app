76b55ed562af38bc62f252a8eb0b498d86e91928
je27 <jocmoya@gmail.com>
2018-02-21 19:49:58 -0600

    initial updated

---
+++ app/controllers/board_words_controller.rb
@@ -0,0 +1,2 @@
+class BoardWordsController < ApplicationController
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+
+  def users_index_action
+    response = Unirest.get ("http://localhost:3000/users")
+    user_hashes = response.body
+    users = []
+    user_hashes.each do |user_hash|
+      users << Users.new(user_hash)
+    end
+    users_index_view(users)
+  end
 end
