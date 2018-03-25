145329bed2e502bd40e7b0c9e9272cc7892ed397
je27 <jocmoya@gmail.com>
2018-03-25 16:57:31 -0500

    added login/logout/signup + a bunch of small design elements

--- public/index.js
+++ public/index.js
@@ -27,7 +27,8 @@ var BoardsIndexPage = {
   methods: {
     setCurrentBoard: function(board) {
       this.currentBoard = board;
-    }
+    },
+  
   },
   computed: {}
 };
@@ -42,9 +43,8 @@ var WordsIndexPage = {
   },
   created: function() {
     axios.get("/words").then(function(response) {
-        console.log(response.data)
-        this.words = response.data;
-      }.bind(this)
+      this.words = response.data;
+    }.bind(this)
     );
   },
   methods: {
@@ -54,6 +54,7 @@ var WordsIndexPage = {
   },
   computed: {}
 };
+
 
 var BoardsShowPage = {
   template: "#boards-show-page",
@@ -85,13 +86,107 @@ var BoardsShowPage = {
     next();
   }
 };
+var WordsShowPage = {
+  template: "#words-show-page",
+  data: function() {
+    return {
+      word: [],
+      currentWord: []
+    };
+  },
+  created: function() {
+    axios.get("/words/" + this.$route.params.id)
+    .then(function(response) {
+    this.word = response.data;
+    }.bind(this));
+  },
+};
+var SignupPage = {
+  template: "#signup-page",
+  data: function() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+        password_confirmation: this.passwordConfirmation
+      };
+      axios
+        .post("/users", params)
+        .then(function(response) {
+          router.push("/login");
+        })
+        .catch(
+          function(error) {
+            this.errors = error.response.data.errors;
+          }.bind(this)
+        );
+    }
+  }
+};
+
+var LoginPage = {
+  template: "#login-page",
+  data: function() {
+    return {
+      email: "",
+      password: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        auth: { email: this.email, password: this.password }
+      };
+      axios
+        .post("/user_token", params)
+        .then(function(response) {
+          axios.defaults.headers.common["Authorization"] =
+            "Bearer " + response.data.jwt;
+          localStorage.setItem("jwt", response.data.jwt);
+          router.push("/");
+        })
+        .catch(
+          function(error) {
+            this.errors = ["Invalid email or password."];
+            this.email = "";
+            this.password = "";
+          }.bind(this)
+        );
+    }
+  }
+};
+var LogoutPage = {
+  created: function() {
+    axios.defaults.headers.common["Authorization"] = undefined;
+    localStorage.removeItem("jwt");
+    router.push("/");
+  }
+};
+
 
 var router = new VueRouter({
   routes: [
   { path: "/", component: BoardsIndexPage },
   { path: "/boards", component: BoardsIndexPage },
   { path: "/words", component: WordsIndexPage },
-  { path: "/boards/:id", component: BoardsShowPage }
+  { path: "/boards/:id", component: BoardsShowPage },
+  { path: "/words/:id", component: WordsShowPage },
+  // { path: "/words/:id/edit", component: WordsEditPage },
+  { path: "/signup", component: SignupPage },
+  { path: "/login", component: LoginPage },
+  { path: "/logout", component: LogoutPage },
+
   ],
   scrollBehavior: function(to, from, savedPosition) {
     return { selector: "div.content", offset : {x: 0, y: 68}};
